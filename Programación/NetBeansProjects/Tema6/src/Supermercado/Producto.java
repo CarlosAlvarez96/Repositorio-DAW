87b1a1e34eb353436a11ca8c47c089a4c27edbf1
CarlosAlvarez96 <calvarezmartin96@gmail.com>
2023-03-09 11:14:40 +0100

    Supermercado

---
+++ Programación/NetBeansProjects/Tema6/src/Supermercado/Producto.java
@@ -0,0 +1,14 @@
+
+package Supermercado;
+
+public record Producto(String nombre, String marca, int precio) {
+    public Producto{
+        if(precio<=0){
+            throw new IllegalArgumentException ("Precio negativo");
+        }
+    }
+    @Override
+        public String toString(){
+            return nombre+" "+marca+" ... "+precio+"€";
+        }
+}
